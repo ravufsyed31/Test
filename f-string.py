b7e5bd9f6d5ad74133d74fd8988db46ec0d3338a
ravufsyed31 <ravuf.u@gmail.com>
2023-07-04 11:59:04 -0700

    f-String

---
+++ f-string.py
@@ -0,0 +1,8 @@
+score = 0
+height = 1.8
+isWinning = True
+print(type(score))
+print(type(height))
+print(type(isWinning))
+#f-String
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
