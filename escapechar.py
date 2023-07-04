929ebc16e2bc4e88bcbf51925cc7ed63b546b1e2
ravufsyed31 <ravuf.u@gmail.com>
2023-07-04 11:46:53 -0700

    escape characters

---
+++ escapechar.py
@@ -0,0 +1,7 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
