84914ae63008b5c1345406be1f3fb5bac697be7a
ravufsyed31 <ravuf.u@gmail.com>
2023-07-04 11:47:45 -0700

    working with strings

---
+++ strings.py
@@ -0,0 +1,22 @@
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greetings = "Hello"
+name = "Tim"
+print(greetings + name)
+
+# if we want a space, we can add that too
+print(greetings + ' ' + name)
+
+
+age = 24
+print(age)
+
+print(type(greetings))
+print(type(age))
+
+age_in_words = "2 years"
+print(type(age))
+print(name + " is " + str(age) + " years old")
